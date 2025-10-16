93188e0473dda9307f239881b8f7ab5dd3ee193f
vit0ur <vitor.engenhariapro@gmail.com>
2025-10-16 10:50:49 -0300

    atualização

---
+++ src/test/java/com/proway/api_intermediary/ApiIntermediaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proway.api_intermediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIntermediaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
